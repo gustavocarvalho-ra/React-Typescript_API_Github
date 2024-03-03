a23a4feff58ac03b6fee4c378ef6c9220f664b99
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-02 21:40:05 -0300

    at3

--- github_search/src/App.tsx
+++ github_search/src/App.tsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom"
 
 function App() {
 
   return (
     <div className="App">
       <h1>GitHub Search</h1>
+      <Outlet />
     </div>
   )
 }
--- github_search/src/main.tsx
+++ github_search/src/main.tsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//Pages
+import Home from "./routes/Home";
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider  router = {router} />
   </React.StrictMode>,
 )
