dd71a09395dca2e0f7713d752eafdffcfdb3bb75
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-03 22:48:48 -0300

    at13

--- github_search/src/components/User.tsx
+++ github_search/src/components/User.tsx
@@ -15,10 +15,12 @@ const User = ({
     <div>
       <img src={avatar_url} alt={login} />
       <h2>{login}</h2>
-      <p>
-        <MdLocationPin />
-        <span>{location}</span>
-      </p>
+      {location && (
+        <p>
+          <MdLocationPin />
+          <span>{location}</span>
+        </p>
+      )}
       <div>
         <div>
           <p>Seguidores:</p>
